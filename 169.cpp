d0144b989a5000e9989cdff50b9720c0b208664f
Ashique Arman <ashiquearman@outlook.com>
2021-05-25 17:14:19 +0600

    Update 169.cpp

--- 169.cpp
+++ 169.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-    int a=0;
-        int count=0;
+        int a = 0 ;
         
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(count == 0) a=i;
-            if(nums[i]==nums[a]) count++;
-            else count--;
+        int c = 0;
+        
+        for(int i=0; i<nums.size(); i++){
             
+            if(c == 0){
+                a = i;
+            }
+            if(nums[i] == nums[a]){
+                c++;
+            }else{
+                c--;
+            }
         }
-        return nums[a];
         
+        return nums[a];
     }
 };
